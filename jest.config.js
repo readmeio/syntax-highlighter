625715984feb4061260ba33b505ac137740ba968
Jon Ursenbach <jon@ursenba.ch>
2020-08-31 15:21:47 -0700

    test: fixing broken tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
-const config = require('../../jest.config');
+const path = require('path');
 
 module.exports = {
-  ...config,
-  rootDir: './',
   coveragePathIgnorePatterns: ['<rootDir>/__tests__/__fixtures__/'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!lib/*.js',
+    '!**/node_modules/**',
+    '!jest.config.js',
+    '!**/coverage/lcov-report/**',
+  ],
+  setupFiles: [path.join(__dirname, '/lib/enzyme')],
   testPathIgnorePatterns: ['<rootDir>/__tests__/__fixtures__/'],
+  transform: {
+    '^.+\\.jsx?$': path.join(__dirname, '/lib/babel-jest'),
+  },
+  transformIgnorePatterns: [
+    // Since `@readme/variable` doesn't ship any transpiled code, we need to transform it as we're running tests.
+    '<rootDir>/node_modules/@readme/variable/^.+\\.jsx?$',
+  ],
 };
