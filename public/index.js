8722c7dc79cd5cf464fbfe386633b775b4970032
Julia Hotaling <42787244+julshotal@users.noreply.github.com>
2020-09-14 15:53:14 -0400

    feat(syntaxhighlighter): line highlighting (#6)
    * feat: attempt at line highlighting w comments
    
    * feat: it works and is less ugly now
    
    * fix: blocking linenumbers from selection
    
    * chore: small eslint fix to pass tests
    
    * feat(highlighting): minor refactor
    - Updated name schemes
    - Reuse components
    
    * feat(highlighting): further refactoring
    - Moving standalone function calls outside parent
    - Consolidating component declaration
    
    * fix(highlighting): applying overlay to deadspace
    
    * chore(coremirror): remove extra class
    
    * chore: adding variable
    
    Co-authored-by: Gabriel Ratcliff <gaberatcliff@gmail.com>
    Co-authored-by: Tony Li <runnabro@users.noreply.github.com>

--- public/index.js
+++ public/index.js
@@ -8,11 +8,25 @@ ReactDOM.render(
     <pre id="hub-reference">
       {syntaxHighlighter(
         `curl --request POST
-        --url http://petstore.swagger.io/v2/pet
+        --url <<url>>
         --header 'authorization: Bearer 123'
         --header 'content-type: application/json'`,
         'curl',
-        { dark: true, highlightMode: true }
+        {
+          dark: true,
+          highlightMode: true,
+          tokenizeVariables: true,
+          ranges: [
+            [
+              { ch: 0, line: 0 },
+              { ch: 0, line: 1 },
+            ],
+            [
+              { ch: 0, line: 3 },
+              { ch: 0, line: 5 },
+            ],
+          ],
+        }
       )}
     </pre>
     <hr />
--- src/codeMirror/index.jsx
+++ src/codeMirror/index.jsx
@@ -1,5 +1,6 @@
 const CodeMirror = require('codemirror');
 const React = require('react');
+const PropTypes = require('prop-types');
 const Variable = require('@readme/variable');
 const { getMode } = require('../utils/modes');
 
@@ -9,10 +10,51 @@ require('codemirror/addon/runmode/runmode');
 require('codemirror/mode/meta.js');
 
 const defaultLineJsx = line => (
-  <p key={`ln-${line}`} className="cm-lineNumber cm-overlay">
+  <p key={`ln-${line}`} className="cm-lineNumber">
     {line}
   </p>
 );
+
+const WrappedLine = ({ className, child }) => <div className={className}>{child}</div>;
+
+WrappedLine.propTypes = {
+  child: PropTypes.any,
+  className: PropTypes.string,
+};
+
+const StructuredOutput = ({ gutteredInput, highlights = [] }) =>
+  gutteredInput.map((ac, idx) => (
+    <WrappedLine
+      key={`cm-wrapped-${idx}`}
+      child={ac}
+      className={['cm-linerow', highlights.length ? highlights[idx] : ''].join(' ')}
+    />
+  ));
+
+StructuredOutput.propTypes = {
+  gutteredInput: PropTypes.any,
+  highlights: PropTypes.arrayOf(PropTypes.string),
+};
+
+const highlightedLines = ranges => {
+  const highlights = [];
+
+  ranges.forEach(([anchor, head]) => {
+    const end = head.line;
+    let position = anchor.line;
+
+    while (position <= end) {
+      highlights[position] = 'cm-highlight';
+      position += 1;
+    }
+  });
+
+  for (let i = 0; i < highlights.length; i += 1) {
+    if (!highlights[i]) highlights[i] = 'cm-overlay';
+  }
+
+  return highlights;
+};
 
 const ReadmeCodeMirror = (code, lang, opts = { tokenizeVariables: false, highlightMode: false, ranges: [] }) => {
   let key = 0;
@@ -61,29 +103,28 @@ const ReadmeCodeMirror = (code, lang, opts = { tokenizeVariables: false, highlig
 
   if (!opts.highlightMode) return output;
 
-  const wrappedOutput = [];
+  const gutteredOutput = [];
   let bucket = [];
 
   output.forEach((o, idx) => {
     const lineBreakRegex = /\n/g;
+
     if (idx === output.length - 1) {
       bucket.push(o);
-      wrappedOutput.push(bucket);
-    } else if (!lineBreakRegex.test(o)) bucket.push(o);
-    else {
-      wrappedOutput.push(bucket);
+      gutteredOutput.push(bucket);
+    } else if (!lineBreakRegex.test(o)) {
+      bucket.push(o);
+    } else {
+      gutteredOutput.push(bucket);
       lineNumber += 1;
       bucket = [defaultLineJsx(lineNumber)];
     }
   });
 
+  const highlights = opts.ranges && opts.ranges.length ? highlightedLines(opts.ranges) : [];
   return (
     <div className="CodeMirror cm-s-material-palenight">
-      {wrappedOutput.map((ac, idx) => (
-        <div key={idx} className="cm-linerow">
-          {ac}
-        </div>
-      ))}
+      <StructuredOutput gutteredInput={gutteredOutput} highlights={highlights} />
     </div>
   );
 };
