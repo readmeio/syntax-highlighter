3bdbe1b54164c86d40d7befcb1ac424a231e489f
Tony Li <runnabro@users.noreply.github.com>
2020-11-17 11:05:33 -0800

    feat(themes): add support for a light theme (#38)

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ import syntaxHighlighter, { cmVariableContext } from '../src/index';
 
 ReactDOM.render(
   <div>
-    <h1>Core Syntax Highlighter</h1>
+    <h1>Core Syntax Highlighter (Dark Theme)</h1>
     <pre id="hub-reference">
       <cmVariableContext.Provider
         value={{
@@ -42,7 +42,45 @@ ReactDOM.render(
       </cmVariableContext.Provider>
     </pre>
     <hr />
-    <h1>Code Editor</h1>
+    <h1>Core Syntax Highlighter (Light Theme)</h1>
+    <pre id="hub-reference">
+      <cmVariableContext.Provider
+        value={{
+          user: {},
+          defaults: [
+            {
+              name: 'url',
+              default: 'GET',
+            },
+          ],
+        }}
+      >
+        {syntaxHighlighter(
+          `curl --request POST
+          --url <<url>>
+          --header 'authorization: Bearer 123'
+          --header 'content-type: application/json'`,
+          'curl',
+          {
+            dark: false,
+            highlightMode: true,
+            tokenizeVariables: true,
+            ranges: [
+              [
+                { ch: 0, line: 0 },
+                { ch: 0, line: 1 },
+              ],
+              [
+                { ch: 0, line: 4 },
+                { ch: 0, line: 5 },
+              ],
+            ],
+          }
+        )}
+      </cmVariableContext.Provider>
+    </pre>
+    <hr />
+    <h1>Code Editor (Dark Theme)</h1>
     <pre id="hub-reference">
       {syntaxHighlighter(
         `curl --request POST
@@ -51,6 +89,18 @@ ReactDOM.render(
         --header 'content-type: application/json'`,
         'curl',
         { editable: true, dark: true }
+      )}
+    </pre>
+    <hr />
+    <h1>Code Editor (Light Theme)</h1>
+    <pre id="hub-reference">
+      {syntaxHighlighter(
+        `curl --request POST
+        --url http://petstore.swagger.io/v2/pet
+        --header 'authorization: Bearer 123'
+        --header 'content-type: application/json'`,
+        'curl',
+        { editable: true, dark: false }
       )}
     </pre>
   </div>,
