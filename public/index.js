c2e334d12bcbd78fd98582849128db17b7a084c5
Jon Ursenbach <erunion@users.noreply.github.com>
2021-09-17 09:41:14 -0700

    feat: upgrading our builds to use webpack 5 (#168)

--- public/index.js
+++ public/index.js
@@ -34,7 +34,7 @@ const exampleJson = `{
   `;
 
 ReactDOM.render(
-  <div>
+  <div style={{ margin: '20px' }}>
     <h1>Core Syntax Highlighter</h1>
 
     <h2>Dark Theme</h2>
@@ -99,37 +99,6 @@ ReactDOM.render(
       </cmVariableContext.Provider>
     </pre>
 
-    <h2>Custom Theme: tomorrow-night</h2>
-    <pre id="hub-reference">
-      <cmVariableContext.Provider
-        value={{
-          user: {},
-          defaults: [
-            {
-              name: 'url',
-              default: 'GET',
-            },
-          ],
-        }}
-      >
-        {syntaxHighlighter(exampleCode, 'curl', {
-          customTheme: 'tomorrow-night',
-          highlightMode: true,
-          tokenizeVariables: true,
-          ranges: [
-            [
-              { ch: 0, line: 0 },
-              { ch: 0, line: 1 },
-            ],
-            [
-              { ch: 0, line: 4 },
-              { ch: 0, line: 5 },
-            ],
-          ],
-        })}
-      </cmVariableContext.Provider>
-    </pre>
-
     <hr />
     <h1>Code Editor</h1>
     <h2>Dark Theme</h2>
@@ -137,11 +106,6 @@ ReactDOM.render(
 
     <h2>Light Theme</h2>
     <pre id="hub-reference">{syntaxHighlighter(exampleCode, 'curl', { editable: true, dark: false })}</pre>
-
-    <h2>Custom Theme: tomorrow-night</h2>
-    <pre id="hub-reference">
-      {syntaxHighlighter(exampleCode, 'curl', { editable: true, customTheme: 'tomorrow-night' })}
-    </pre>
 
     <hr />
     <h1>Code Folding</h1>
@@ -153,11 +117,6 @@ ReactDOM.render(
     <h2>Light Theme</h2>
     <pre id="hub-reference">
       {syntaxHighlighter(exampleJson, 'json', { foldGutter: true, readOnly: true, dark: false })}
-    </pre>
-
-    <h2>Custom Theme: tomorrow-night</h2>
-    <pre id="hub-reference">
-      {syntaxHighlighter(exampleJson, 'json', { foldGutter: true, readOnly: true, customTheme: 'tomorrow-night' })}
     </pre>
   </div>,
   document.getElementById('root')
